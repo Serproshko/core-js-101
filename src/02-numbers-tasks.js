4b99414367d03635648249a36ffc1d01732d0d75
Sergey <sergeyproshko2005@gmail.com>
2024-10-02 13:29:25 +0300

    feat: 48 / 76 tasks complited!

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -226,8 +226,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if ((n % 2 === 0 && n !== 2) || (n % 3 === 0 && n !== 3)
+  || (n % 5 === 0 && n !== 5) || (n % 7 === 0 && n !== 7)) {
+    return false;
+  }
+  return true;
 }
 
 /**
